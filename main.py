8e7dbb126a41c8a7da90b06d8fb487b69c63330a
Akram Chaouache <akramchaouache@gmail.com>
2023-10-30 16:35:00 +0100

    added main

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from stable_baselines3 import PPO
+from matrix import MatrixEnv
+
+env = MatrixEnv()
+
+model_path = os.path.join('Training', 'models')
+model = PPO.load(model_path, env=env)
+
+obs = env.reset()
+env.render('human', "Initial state")
+
+for _ in range(20):  # Adjust the number of steps for testing
+    action, _ = model.predict(obs)
+    obs, _, done, _ = env.step(action)
+      # Optional: visualize the agent's actions
+env.render('human', "After 20 moves")
